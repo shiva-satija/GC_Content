623e1efc6a235071d307c03691d63a5cd1c40d08
Cricket <errcricket@gmail.com>
2013-05-24 17:27:51 -0400

    Concatenate multiple lines of a single sequences into a single line

---
+++ Soil/HCH/concateLines.py
@@ -0,0 +1,43 @@
+'''
+Created:  E. Reichenberger
+Date:     5.24.2013
+
+Purpose:  Concatenate multiple lines of a single sequences into a single line
+'''
+
+import os
+import re
+import sys
+import glob
+
+
+arguments = sys.argv
+
+inputFastaFile = arguments[1]
+inputfastaFile = inputFastaFile.replace(' ', '')
+
+inputFile = open(inputFastaFile, 'r')
+outputFile = open('temp.fa', 'w')
+
+fastalines = inputFile.readlines()
+
+sequence = ''
+
+for lines in fastalines:
+     lines = lines.replace('\n', '')
+     if lines.startswith('>'):
+          if sequence != '':
+               outputFile.write(sequence + '\n')
+               sequence = ''
+          flag = 0
+          outputFile.write(lines + '\n')
+          sequence = ''
+     else: 
+          flag = 1
+          if flag == 1:
+               sequence = sequence + lines
+
+inputFile.close()
+outputFile.close()
+
+os.rename('temp.fa', inputFastaFile)
