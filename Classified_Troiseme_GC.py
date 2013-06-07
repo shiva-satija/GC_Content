382621cee10c3493a962deda4552e94452f6d0e2
Cricket <errcricket@gmail.com>
2013-06-07 17:59:39 -0400

    Somehow the order between the original fasta file and the results PhymmBL do not align. Re-writing script to account for this -- however, this will be much slower.  11 If it is possible to revert to the previous method, the "count" variable would need to be re-introduced (start value == 1, first line in PhymmBL results file is a header) and the  12 increment count for each time

--- Classified_Troiseme_GC.py
+++ Classified_Troiseme_GC.py
@@ -6,6 +6,10 @@ Purpose: To merge classified sequences file to original fasta file into one whil
 
 Addendum 4.24.2013: Add ability to add filename to end of python call: python test.py arg1 arg2 arg3
 and call the script from a bash script.
+
+Addendum: Somehow the order between the original fasta file and the results PhymmBL do not align. Re-writing script to account for this -- however, this will be much slower.
+If it is possible to revert to the previous method, the "count" variable would need to be re-introduced (start value == 1, first line in PhymmBL results file is a header) and the 
+increment count for each time '>' is found in the original fasta file.
 '''
 
 import Bio
@@ -22,56 +26,52 @@ from Bio.SeqUtils import GC
 
 arguments = sys.argv
 #print arguments
-#['/data/erin/Ruti/TroisiemeCodon_Position/Classified_Troiseme_GC.py', 'Diamond_fork_biofilm_4460448.fa', '/home/erin/Ruti/TroisiemeCodon_Position/ENVIRONMENTS/Water/Fresh/Stream/Diamond_fork_biofilm_4460448/Phylum/results.03.phymmBL__Diamond_fork_biofilm_4460448_fa.txt', 'classified_GC_Diamond_fork_biofilm_4460448.fa']
 
-fastaSequenceFileName = arguments[1] #This is the original fasta (unsplit) file
-fastaSequenceFileName = fastaSequenceFileName.replace(' ', '')
-resultsPhymmBL_fileName = arguments[2] #This is the results_03_PhymmBL... file
-resultsPhymmBL_fileName = resultsPhymmBL_fileName.replace(' ', '')
-classifiedFileName = arguments[3] #This is the name of the output file
+originalFastaFile = arguments[1] #This is the original fasta (unsplit) file
+originalFastaFile = originalFastaFile.replace(' ', '')
+PhymmBLresultsFile = arguments[2] #This is the results_03_PhymmBL... file
+PhymmBLresultsFile = PhymmBLresultsFile.replace(' ', '')
+mergedFile = arguments[3] #This will be the name of the output file
 
-results_content = open(resultsPhymmBL_fileName, 'r')
-results_lines = []
-resultsFiles = results_content.readlines()
+inputResults = open(PhymmBLresultsFile, 'r')
+resultLines = inputResults.readlines() #readlines() reads in entire file at once.
+resultArray = []
 
-for results in resultsFiles:
-	results_lines.append(results)
+for results in resultLines:
+	tempResults = results.split('\t',1)
+	results = tempResults[0]
+	resultArray.append(results)
 
-results_content.close()
+inputResults.close()
 
 ########################Removing Old Files###############################
-if os.path.exists(classifiedFileName):
-	os.remove(classifiedFileName)
-outputFile = open(classifiedFileName, 'a')
+if os.path.exists(mergedFile):
+	os.remove(mergedFile)
+outputFile = open(mergedFile, 'a')
 
 outputFile.write('#QUERY_ID' + '\t' + 'BEST_MATCH' + '\t' + 'SCORE' + '\t' + 'GENUS' + '\t' + 'GENUS_CONF' + '\t' + 'FAMILY' + '\t' + 'FAMILY_CONF' + '\t' + 'ORDER' + '\t' + 'ORDER_CON' + '\t' + 'CLASS' + '\t' + 'CLASS_CONF' + '\t' + 'PHYLUM' + '\t' + 'PHYLUM_CONF' + '\t' + 'GC_1' + '\t' + 'GC_2' + '\t' + 'GC_3' + '\t' + 'GC_overall' + '\n')
 
-inputFasta_fileLines = open(fastaSequenceFileName, 'r')
-Fasta_fileLines = inputFasta_fileLines.readlines()
+inputFasta = open(originalFastaFile, 'r')
+fastaLines = inputFasta.readlines() #readlines() reads in entire file at once.
 fastaArray = []
-resultsArray = []
-count = 1
-for i in range(0, len(Fasta_fileLines) - 1):
-	lines = Fasta_fileLines[i]
+
+id_Fasta = ''
+
+for i in range(0, len(fastaLines) - 1): #from 0 to the number of lines in the file minus one
+	lines = fastaLines[i]
 	if lines.startswith('>'):
-		id_Fasta = lines.strip('>')
-		id_Fasta = id_Fasta.strip('\n')
-		Strings = str(results_lines[count])
-		fastaArray.append(Strings)
-		results_lines[count] = results_lines[count].strip('\n')
+		id_Fasta = lines.strip('>') #strip removes leading and trailing characters
+		id_Fasta = id_Fasta.strip('\n') #strip removes leading and trailing characters
+		id_Fasta = id_Fasta.strip('\s') #strip removes leading and trailing characters
+		
+		#if resultLines[count].startswith(id_Fasta):
+		count = resultArray.index(id_Fasta)
+		fastaLines[i+1] = fastaLines[i+1].strip('\n')
+		gc_1 = round(GC123(fastaLines[i+1])[1], 2)
+		gc_2 = round(GC123(fastaLines[i+1])[2], 2)
+		gc_3 = round(GC123(fastaLines[i+1])[3], 2)
+		gc_overall = round(GC(fastaLines[i+1]), 2)
+		outputFile.write('>' + resultLines[count].strip('\n')  + '\t' + str(gc_1) + '\t' + str(gc_2) + '\t' + str(gc_3) + '\t' + str(gc_overall) + '\n')
 
-	if Strings.startswith(id_Fasta):
-		resultsArray.append(Strings)
-		gc_1 = round(GC123(Fasta_fileLines[i+1])[1], 2)
-		gc_2 = round(GC123(Fasta_fileLines[i+1])[2], 2)
-		gc_3 = round(GC123(Fasta_fileLines[i+1])[3], 2)
-		gc_overall = round(GC(Fasta_fileLines[i+1]), 2)
-		outputFile.write('>' + results_lines[count] + '\t' + str(gc_1) + '\t' + str(gc_2) + '\t' + str(gc_3) + '\t' + str(gc_overall) + '\n')
-		outputFile.write(Fasta_fileLines[i+1])
-		count+=1
-
-print len(fastaArray)
-print len(resultsArray)
-
-inputFasta_fileLines.close()
+inputFasta.close()
 outputFile.close()
