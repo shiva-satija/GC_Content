a0815d5c8486446ae8426afe5282bf075dd93e27
Cricket <errcricket@gmail.com>
2013-06-17 14:49:20 -0400

    Cleaning up file

--- moveResultFiles.py
+++ moveResultFiles.py
@@ -13,14 +13,14 @@ For example:
 This script will be called from a bash script where the arguments will be defined within the bash script.
 
 Below are the potential types of files
-errFile__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_59_fa.txt
-rawBlastOutput__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_00_fa.txt
-rawPhymmOutput__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_63_fa.txt
-results.01.phymm__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_00_fa.txt
-results.02.blast__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_00_fa.txt
-results.03.phymmBL__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_03_fa.txt
-tempRev__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_59_fa.txt
-tempRev__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904.3_Phylum_4EB024_r1_4477904.3
+errFile__home_...fileName_noExt
+rawBlastOutput__home_...fileName_noExt
+rawPhymmOutput__home_...fileName_noExt
+results.01.phymm__home_...fileName_noExt
+results.02.blast__home_...fileName_noExt
+results.03.phymmBL__home_...fileName_noExt
+tempRev__home_...fileName_noExt
+tempRev__home_...fileName_noExt
 
 '''
 
@@ -44,8 +44,6 @@ fileName_noExt = fileName_noExt.replace('.', '_')
 
 for i in range(len(arguments)):
 	if i > 2 and i < 10:
-		#arguments[i] = arguments[i].replace('.', '')
-		#arguments[i] = arguments[i].replace('/', '')
 		arguments[i] = path + '/' +  arguments[i] + '_' + scoreRead_prefix + '_' + fileName_noExt
 
 errFile = arguments[3]  
@@ -59,28 +57,12 @@ tempRev = arguments[9]
 infiles = []
 outfiles = []
 
-count = 0
 for infile in glob.glob(os.path.join(path, '*.txt') ): #file extension type
 	infiles.append(infile)
 	if infile.startswith(errFile) or infile.startswith(rawBlastOutput) or infile.startswith(rawPhymmOutput) or infile.startswith(results1) or infile.startswith(results2) or infile.startswith(results3) or infile.startswith(tempRev):
-		count+=1
-	
-print len(infiles)
-
-#for files in infiles:	
-#	if files.startswith(errFile) or files.startswith(rawBlastOutput) or files.startswith(rawPhymmOutput) or files.startswith(results1) or files.startswith(results2) or files.startswith(results3) or files.startswith(tempRev):
-#		count+=1
-#	else:
-#		print files
-
-print count
-'''
-     infiles.append(infile)
-     if scoreRead_prefix in infile:   #Alternative: if not tempString in files:
-          newfile = infile.replace(path, '')
-          newfile = newfile.replace('/', '')
-          newfile = newfile.replace(scoreRead_prefix, '')
-          newfile = newfile.replace(environmentDirectory_prefix, '')
-          outfiles.append(newfile)
-          os.rename(infile, 'output/'+ newfile)
-'''
+		#newfile = infile.replace(environmentDirectory_prefix, '')
+		newfile = infile.replace(path, '')
+		#newfile = newfile.replace(scoreRead_prefix, '')
+		#outfiles.append(newfile)
+		#print newfile
+		os.rename(infile, 'output/'+ newfile)
