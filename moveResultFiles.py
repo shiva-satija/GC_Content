c38023494a0e68ce4f1e86f8a66ff85bfcfabcb6
Cricket <errcricket@gmail.com>
2013-06-17 14:32:40 -0400

    Updating file names

--- moveResultFiles.py
+++ moveResultFiles.py
@@ -7,13 +7,9 @@ Purpose: Move specific text files from the PhymmBL to the current working direct
      in the PhymmBL without discrimination would potentially move some text files to the wrong directory.
 
 Call file like so:
-python moveResultFiles.py $PWD $PhymmBL_directory $Environment_directory
+python $scriptPathway/moveResultFiles.py $PWD $PhymmBL_directory $Environment_Directory $errFile $rawBlastOutput $rawPhymmOutput $results1 $results2 $results3 $tempRev $fileName_noExt
 
 For example:
-00
-PWD = "/data/erin/Ruti/TroisiemeCodon_Position/ENVIRONMENTS/Soil/Prairie/4502935.3"
-PhymmBL_directory="/data/erin/Ruti/TroisiemeCodon_Position/PhymmBL"
-Environment_directory="/data/erin/Ruti/TroisiemeCodon_Position/ENVIRONMENTS/"         
 This script will be called from a bash script where the arguments will be defined within the bash script.
 
 Below are the potential types of files
@@ -24,6 +20,7 @@ results.01.phymm__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Deser
 results.02.blast__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_00_fa.txt
 results.03.phymmBL__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_03_fa.txt
 tempRev__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904_3_Phylum_4EB024_r1_4477904_3_59_fa.txt
+tempRev__home_erin_Ruti_TroisiemeCodon_Position_ENVIRONMENTS_Soil_Desert_4EB024_r1_4477904.3_Phylum_4EB024_r1_4477904.3
 
 '''
 
@@ -32,31 +29,52 @@ import glob
 import sys
 import re #regular expressions
 import shutil
-#python $scriptPathway/moveResultsFiles.py $PWD $PhymmBL_directory $Environment_Directory $errFile $rawBlastOutput $rawPhymmOutput $results1 $results2 $results3 $tempRev 
 
 arguments = sys.argv
-print arguments
 workingDirectory = arguments[1] #$PWD will be the current working directory
 workingDirectory = workingDirectory.replace('data', 'home') #PWD uses 'home' rather than 'data'
-#workingDirectory = workingDirectory.replace('home', 'data') #PWD uses 'home' rather than 'data'
 scoreRead_prefix = workingDirectory.replace('/', '_')
+scoreRead_prefix = scoreRead_prefix.replace('.', '_')
 
 path = arguments[2] #$PhymmBL_directory where (where results from the classifier are created)
-environmentDirectory = arguments[3]
+environmentDirectory = arguments[1]
 environmentDirectory_prefix = environmentDirectory.replace('/', '_')
+fileName_noExt=arguments[10]
+fileName_noExt = fileName_noExt.replace('.', '_')
 
-errFile = arguments[4]
-rawBlastOutput = arguments[5]
-rawPhymmOutput = arguments[6]
-results1 = arguments[7]
-results2 = arguments[8]
-results3 = arguments[9]
-tempRev = arguments[10]
+for i in range(len(arguments)):
+	if i > 2 and i < 10:
+		#arguments[i] = arguments[i].replace('.', '')
+		#arguments[i] = arguments[i].replace('/', '')
+		arguments[i] = path + '/' +  arguments[i] + '_' + scoreRead_prefix + '_' + fileName_noExt
+
+errFile = arguments[3]  
+rawBlastOutput = arguments[4] 
+rawPhymmOutput = arguments[5]
+results1 = arguments[6]
+results2 = arguments[7]
+results3 = arguments[8]
+tempRev = arguments[9]
 
 infiles = []
 outfiles = []
 
+count = 0
 for infile in glob.glob(os.path.join(path, '*.txt') ): #file extension type
+	infiles.append(infile)
+	if infile.startswith(errFile) or infile.startswith(rawBlastOutput) or infile.startswith(rawPhymmOutput) or infile.startswith(results1) or infile.startswith(results2) or infile.startswith(results3) or infile.startswith(tempRev):
+		count+=1
+	
+print len(infiles)
+
+#for files in infiles:	
+#	if files.startswith(errFile) or files.startswith(rawBlastOutput) or files.startswith(rawPhymmOutput) or files.startswith(results1) or files.startswith(results2) or files.startswith(results3) or files.startswith(tempRev):
+#		count+=1
+#	else:
+#		print files
+
+print count
+'''
      infiles.append(infile)
      if scoreRead_prefix in infile:   #Alternative: if not tempString in files:
           newfile = infile.replace(path, '')
@@ -65,3 +83,4 @@ for infile in glob.glob(os.path.join(path, '*.txt') ): #file extension type
           newfile = newfile.replace(environmentDirectory_prefix, '')
           outfiles.append(newfile)
           os.rename(infile, 'output/'+ newfile)
+'''
