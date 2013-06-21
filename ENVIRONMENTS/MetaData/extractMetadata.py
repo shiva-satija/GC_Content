6b562fb83d7ea4490473f5db41e71582593bc458
Cricket <errcricket@gmail.com>
2013-06-21 17:08:11 -0400

    Implementing def. to strip lines of supplied words

--- ENVIRONMENTS/MetaData/extractMetadata.py
+++ ENVIRONMENTS/MetaData/extractMetadata.py
@@ -30,6 +30,7 @@ project_organization_url = ''
 project_PI_firstName = ''
 project_PI_lastName = ''
 project_PI_email = ''
+project_PI_organization_url = ''
 project_NCBI = ''
 
 ####Sample
@@ -67,7 +68,7 @@ for line in lines:
 	fileList.append(line)
 
 outputFile.write('project_ID\t	project_name\t	project_description\t	project_sampleFeature\t	project_sampleBiome\t' + \
-	'project_organization\t	project_PI\t	project_PI_email\t	project_NCBI\t	project_collectionDate\t' + \
+	'project_organization\t	project_PI\t	project_PI_email\t	PI_organization_url\t	project_NCBI\t	project_collectionDate\t' + \
 	'project_collectionTimeZone\t	location_coordinates\t	project_location\t	project_sampleCountry\t' + \
 	'project_sampleContinent\t	project_depth\t	project_temperature\t	project_pH\t	project_salinity\t' + \
 	'project_sequencingMethod\t	project_dataType\t	project_pubMedID\t	project_link\t 	project_metatdataLink\n')
@@ -76,124 +77,61 @@ for files in fileList:
 	inputF = open(files, 'r')
 	lines = inputF.readlines()
 
-def removeInput(fileLine, searchWord): #strip word (searchWord) from file (fileLine) & return the new line
 	for line in lines:
 		#####Project
 		if line.startswith('Project'):
 			line = removeInput(line, 'Project')
 			if line.startswith('project_name'):
-				projec
-				line = line.replace('project_name', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_name = line
+				project_name = removeInput(line, 'project_name') 
 			if line.startswith('project_description'):
-				line = line.replace('project_description', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_description = line
+				project_description = removeInput(line, 'project_description')
 			if line.startswith('project_sampleFeature'):
-				line = line.replace('project_sampleFeature', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_sampleFeature = line
+				project_sampleFeature = removeInput(line, 'project_sampleFeature')
 			if line.startswith('project_sampleBiome'):
-				line = line.replace('project_sampleBiome', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_sampleBiome = line
+				project_sampleBiome = removeInput(line, 'project_sampleBiome')
 			if line.startswith('project_organization') and line.startswith(project_organization_url[0]) == False and line.startswith('PI_organization_address') == False and line.startswith('PI_organization_country') == False:
-				line = line.replace('project_organization', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_organization = line
-			if line.startswith('project_PI_firstName'):
-				line = line.replace('project_PI_firstName', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_PI_firstName = line
-			if line.startswith('project_PI_lastName'):
-				line = line.replace('project_PI_lastName', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_PI_lastName = line
-			if line.startswith('project_PI_email'):
-				line = line.replace('project_PI_email', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_PI_email = line
+				project_organization = removeInput(line, 'project_organization')
+			if line.startswith('PI_firstname'):
+				project_PI_firstName = removeInput(line, 'PI_firstname')
+			if line.startswith('PI_lastname'):
+				project_PI_lastName = removeInput(line, 'PI_lastname')
+			if line.startswith('PI_email'):
+				project_PI_email = removeInput(line, 'PI_email') 
+			if line.startswith('PI_organization_url'):
+				project_PI_organization_url = removeInput(line, 'PI_organization_url') 
 
 		####Sample
 		if line.startswith('Sample'):
-			line = str(line.replace('Sample', ''))
-			line = str(line.lstrip())
-			line = str(line.rstrip())
-			line = line.replace('\n', '')
+			line = removeInput(line, 'Sample')
 			if line.startswith('feature'):
 				line = line.replace('feature', '')
 				line = str(line.lstrip())
 				line = str(line.rstrip())
 				project_feature = line
 			if line.startswith('collection_date'):
-				line = line.replace('collection_date', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_collectionDate = line
+				project_collectionDate = removeInput(line, 'collection_date')
 			if line.startswith('collection_timezone'):
-				line = line.replace('collection_timezone', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_collectionTimeZone = line
+				project_collectionTimeZone = removeInput(line, 'collection_timezone')
 			if line.startswith('project_sampleSize'):
-				line = line.replace('projecct_sampleSize', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_sampleSize = line
+				project_sampleSize = removeInput(line, 'project_sampleSize')
 			if line.startswith('latitude'):
-				line = line.replace('latitude', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_latitude = line
+				project_latitude = removeInput(line, 'latitude')
 			if line.startswith('longitude'):
-				line = line.replace('longitude', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_longitude = line
+				project_longitude = removeInput(line, 'longitude') 
 			if line.startswith('location'):
-				line = line.replace('location', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_location = line
+				project_location = removeInput(line, 'location')
 			if line.startswith('continent'):
-				line = line.replace('continent', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_sampleContinent = line
+				project_sampleContinent = removeInput(line, 'continent')
 			if line.startswith('depth'):
-				line = line.replace('depth', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_depth = line
+				project_depth = removeInput(line, 'depth')
 			if line.startswith('temperature'):
-				line = line.replace('temperature', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_temperature = line
+				project_temperature = removeInput(line, 'temperature')
 			if line.startswith('pH'):
-				line = line.replace('pH', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
-				project_pH = line
+				project_pH = removeInput(line, 'pH')
 			if line.startswith('salinity') or line.startswith('misc_param'):
-				line = line.replace('misc_param', '')
-				line = str(line.lstrip())
-				line = line.replace('salinity', '')
-				line = str(line.lstrip())
-				line = str(line.rstrip())
+				line = removeInput(line, 'misc_param')
+				line = removeInput(line, 'salinity')
 				project_salinity = line
-			Enviromental Package: water       salinity        27-30%
-			Sample    salinity        9
-			Sample    misc_param      salinity: 6-8 ; Percent
 
 		if line.startswith('Enviromental Package:') or line.startswith('Sample'):
 			line = line.replace('Environmental', '')
@@ -226,7 +164,7 @@ def removeInput(fileLine, searchWord): #strip word (searchWord) from file (fileL
 
 	outputLink = str(project_link + files + '\t' + metadata_link + files + '.metadata.txt\n')
 	outputFile.write(files + '\t' + project_name + '\t' + project_description + '\t' + project_sampleFeature + '\t' + project_sampleBiome + '\t' + \
-		project_organization + '\t' +  project_PI_firstName + ' ' + project_PI_lastName + '\t' + project_PI_email + '\t' + project_NCBI + '\t' + \
+		project_organization + '\t' +  project_PI_firstName + ' ' + project_PI_lastName + '\t' + project_PI_email + '\t' + project_PI_organization_url + '\t' + project_NCBI + '\t' + \
 		project_collectionDate + '\t' + project_collectionTimeZone + '\t' + project_latitude + ',' + project_longitude + '\t' + \
 		project_location + '\t' + project_sampleCountry + '\t' + project_sampleContinent + '\t' + \
 		project_depth + '\t' + project_temperature  + '\t' + project_pH + '\t' + project_salinity + '\t' + project_sequencingMethod + '\t' + \
