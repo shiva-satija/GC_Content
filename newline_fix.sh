9e8280af3811cffdda4d3d31c2ba7f5e4fa64321
Calvin Morrison <mutantturkey@gmail.com>
2013-07-17 09:54:37 -0400

    cleanup newline_fix function

--- newline_fix.sh
+++ newline_fix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-filename="f"
 
+# This function fixes files if they do not end with a \n character.
 function fix_newline {
 last_char=$(tail -c 1 "$1"; printf x); 
 last_char=${last_char%x}
@@ -9,6 +9,3 @@ if [ "$last_char" != $'\n' ]; then
   printf "\n" >> $filename
 fi
 }
-  
-fix_newline $filename
-
