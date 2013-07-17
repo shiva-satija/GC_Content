3a836eb565ae37456e8f92ee32c9e6bf7d1b1cb8
Calvin Morrison <mutantturkey@gmail.com>
2013-07-17 09:52:48 -0400

    a mythical solution to the newline problem

--- newline_fix.sh
+++ newline_fix.sh
@@ -2,9 +2,10 @@
 filename="f"
 
 function fix_newline {
-last_char=`tail -c 1 $1`
+last_char=$(tail -c 1 "$1"; printf x); 
+last_char=${last_char%x}
 
-if [[ $last_char='\n' ]]; then
+if [ "$last_char" != $'\n' ]; then
   printf "\n" >> $filename
 fi
 }
